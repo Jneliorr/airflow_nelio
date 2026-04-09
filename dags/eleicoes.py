8eb41443b0f8b197ce7e940a46b7e887cebc03f0
Jose Nelio Bezerra  Maciel Junior <jneliorr@gmail.com>
2026-04-08 22:00:10 -0300

    adicionado eleicao

---
+++ dags/eleicoes.py
@@ -0,0 +1,299 @@
+from airflow.sdk import dag, task
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import Param
+from airflow.sdk.definitions.param import ParamsDict
+from sqlalchemy import create_engine, VARCHAR, Integer,Date
+import os
+import pandas as pd
+from datetime import datetime
+import zipfile
+import numpy as np
+import requests
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, RenderConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+
+
+# Capturando as variáveis do seu ambiente Docker
+USER = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
+
+params = {
+        "Usuario": Param(
+        default="José Nélio"
+        ,type='string'
+        ,enum=["José Nélio"]
+        ,description="""
+        Escolher usuario 
+        """
+        )
+    # ,'anos': Param(
+    #         default='2026-02'
+    #         ,type="string"
+    #         ,description="Escolher ano ou Todos: 2026 ou 2026-02 ou TODOS")
+    # ,'estados': Param(
+    #         default='RR'
+    #         ,type="string"
+    #         ,description="Estado Filtrado")
+    ,'database': Param(
+        default='eleicao_RR'
+        ,type="string"
+        ,description="Escolher o nome do banco de archives para subir os archives tratados")
+
+    }
+
+# Configuração de onde o dbt está no seu Docker/Worker
+DBT_PROJECT_PATH = Path("/opt/airflow/dbt/eleicao")
+
+profile_config = ProfileConfig(
+    profile_name="eleicao",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="postgres_default", # O ID da conexão que você cria no Airflow UI
+        profile_args={"schema": "public"},
+    ),
+)
+
+
+default_args = {
+    "owner": "Nelio Cruel",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+@dag(
+    dag_id="dag_eleicao",
+    start_date=datetime(2024, 1, 1),
+    schedule="@once", 
+    params=params,
+    catchup=False,
+    tags=["cinema"],
+    max_active_tasks= 30,
+    default_args=default_args
+)
+
+
+def eleicao():
+    
+    @task
+    def unzip_file(zip_path, file_save):
+        os.makedirs(file_save, exist_ok=True)
+
+        for file_name in os.listdir(zip_path):
+            if file_name.lower().endswith('.zip'):
+                zip_file = os.path.join(zip_path, file_name)
+                with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                    for member in zip_ref.namelist():
+                        nome_arquivo = os.path.basename(member)
+                        destino_final = os.path.join(file_save, nome_arquivo)
+                        with zip_ref.open(member) as origem, open(destino_final, 'wb') as destino:
+                            shutil.copyfileobj(origem, destino)
+                        print(f"Extraído: {nome_arquivo}")
+
+
+
+    # @task
+    # def local_votacao (path):
+    #     engine = create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@db:5432/eleicao_RR")
+    #     dfs = []
+    #     colunas_num = ['QT_ELEITOR_SECAO', 'QT_ELEITOR_ELEICAO_FEDERAL', 'QT_ELEITOR_ELEICAO_ESTADUAL','QT_ELEITOR_ELEICAO_MUNICIPAL']
+    #     colunas_data = ['DT_GERACAO','DT_ELEICAO',]
+    #     colunas_hora = ['HH_GERACAO']
+    #     for bweb in os.listdir(path):
+    #         full_path = os.path.join(path, bweb)
+    #         if full_path.endswith('.csv'):
+    #             print(f"Lendo o arquivo: {full_path}")
+    #             df = pd.read_csv(full_path, encoding='latin-1', sep=';',dtype=str,nrows=10)
+    #             for col in colunas_num:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_numeric(df[col]).astype('Int64')
+    #             for col in colunas_data:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col],  dayfirst=True, format='%d/%m/%Y').dt.date
+    #             for col in colunas_hora:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col]).dt.time
+    #             df['AA_ELEICAO'] = df['AA_ELEICAO'].astype(str).str.lstrip("0")
+    #             df['ID_ZONA_SECAO_MUNICIPIO'] = df['AA_ELEICAO'] +  df['NR_TURNO'] + df['NR_ZONA'] +  df['NR_SECAO'] +  df['CD_MUNICIPIO']
+    #             dfs.append(df)
+    #             print(f"Carregando o arquivo no banco de archives: {bweb}")
+    #             df.to_sql("local_votacao", engine, index=False, if_exists='append')
+    #             print(f"Arquivo Carregado no Bando de archives: {bweb}")
+
+
+    #     return print("Todos os arquivos foram processados e carregados no banco de archives com sucesso!")
+
+    # @task
+    # def perfil (file_csv):
+    #     engine = create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@db:5432/eleicao_RR")
+    #     dfs = []
+    #     colunas_num = ['QT_ELEITORES_INC_NM_SOCIAL', 'QT_ELEITORES_DEFICIENCIA', 'QT_ELEITORES_BIOMETRIA','QT_ELEITORES_PERFIL']
+    #     colunas_data = ['DT_GERACAO']
+    #     # colunas_dataHora = ['DT_BU_RECEBIDO','DT_CARGA_URNA_EFETIVADA','DT_ABERTURA','DT_ENCERRAMENTO','DT_EMISSAO_BU']
+    #     colunas_hora = ['HH_GERACAO']
+    #     for bweb in os.listdir(file_csv):
+    #         full_path = os.path.join(file_csv, bweb)
+    #         if full_path.endswith('.csv'):
+    #             print(f"Lendo arquivo: {full_path}")
+    #             df = pd.read_csv(full_path, encoding='latin-1', sep=';',dtype=str,nrows=10)
+    #             for col in colunas_num:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_numeric(df[col]).astype('Int64')
+    #             for col in colunas_data:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col],  dayfirst=True, format='%d/%m/%Y').dt.date
+    #             for col in colunas_hora:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col]).dt.time
+    #             df['ID_ZONA_SECAO_MUNICIPIO'] = df['ANO_ELEICAO'] + df['NR_ZONA'] +  df['NR_SECAO'] +  df['CD_MUNICIPIO']
+    #             dfs.append(df)
+    #             print(f"Carregando o arquivo no banco de archives: {bweb}")
+    #             df.to_sql("perfil_eleitorado", engine, index=False, if_exists='append')
+    #             print(f"Arquivo Carregado no Bando de archives: {bweb}")
+
+    #     return print("Todos os arquivos foram processados e carregados no banco de archives com sucesso!")
+    
+
+    # @task
+    # def turnos (file_csv):
+    #     engine = create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@db:5432/eleicao_RR")
+    #     dfs = []
+    #     colunas_num = ['NR_TURMA_APURADORA', 'NR_JUNTA_APURADORA', 'QT_VOTOS', 'QT_APTOS', 'QT_COMPARECIMENTO', 'QT_ABSTENCOES']
+    #     colunas_data = ['DT_GERACAO', 'DT_PLEITO']
+    #     colunas_dataHora = ['DT_BU_RECEBIDO','DT_CARGA_URNA_EFETIVADA','DT_ABERTURA','DT_ENCERRAMENTO','DT_EMISSAO_BU']
+    #     colunas_hora = ['HR_GERACAO']
+    #     for bweb in os.listdir(file_csv):
+    #         full_path = os.path.join(file_csv, bweb)
+    #         if full_path.endswith('.csv'):
+    #             df = pd.read_csv(full_path, encoding='latin-1', sep=';',dtype=str,nrows=10)
+    #             for col in colunas_num:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_numeric(df[col], errors='coerce').astype('Int64')
+    #             for col in colunas_data:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col], errors='coerce').dt.date
+    #             for col in colunas_hora:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col], errors='coerce').dt.time
+    #             for col in colunas_dataHora:
+    #                 if col in df.columns:
+    #                     df[col] = pd.to_datetime(df[col], errors='coerce')
+    #             df['ID_ZONA_SECAO_MUNICIPIO'] = df['ANO_ELEICAO'] + df['NR_TURNO'] + df['NR_ZONA'] +  df['NR_SECAO'] +  df['CD_MUNICIPIO']
+    #             df['NM_CANDIDATO'] = (
+    #                 df['NM_VOTAVEL']
+    #                 .fillna('')
+    #                 .astype(str)
+    #                 .str.normalize('NFKD')
+    #                 .str.encode('ascii', 'ignore')
+    #                 .str.decode('utf-8')
+    #                 .str.replace(r'[^A-Za-z0-9]+', '', regex=True)
+    #             )
+    #             df['ID_CANDIDATO'] = df['ANO_ELEICAO'] + df['NR_TURNO'] +  df['CD_CARGO_PERGUNTA'] + df['CD_MUNICIPIO'] +  df['NR_VOTAVEL'] + df['NM_CANDIDATO']
+    #             dfs.append(df)
+    #             print(f"Carregando o arquivo no banco de archives: {bweb}")
+    #             df.to_sql("resultado", engine, index=False, if_exists='append')
+    #             print(f"Arquivo Carregado no Bando de archives: {bweb}")
+
+    #     return print("Todos os arquivos foram processados e carregados no banco de archives com sucesso!")
+    @task
+    def unzip_candidato(zip_path, file_save):
+        os.makedirs(file_save, exist_ok=True)
+
+        for file_name in os.listdir(zip_path):
+            if file_name.lower().endswith('.zip'):
+                zip_file = os.path.join(zip_path, file_name)
+                with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                    for member in zip_ref.namelist():
+                        if member.endswith('/') or '_RR.csv' not in os.path.basename(member):
+                            continue
+                        nome_arquivo = os.path.basename(member)
+                        destino_final = os.path.join(file_save, nome_arquivo)
+                        with zip_ref.open(member) as origem, open(destino_final, 'wb') as destino:
+                            shutil.copyfileobj(origem, destino)
+                        print(f"Extraído: {nome_arquivo}")
+
+
+
+    unzip_csv_candidato_RR = unzip_candidato.override(task_id="unzip_csv_candidatos_RR")(
+        zip_path = "/opt/airflow/TRE2026/archives/RR/candidato/zip-csv",
+        file_save="/opt/airflow/TRE2026/archives/RR/candidato/csv"
+    )
+    unzip_foto_candidato_RR = unzip_file.override(task_id="unzip_foto_candidatos_RR")(
+    zip_path = "/opt/airflow/TRE2026/archives/RR/candidato/zip-fotos",
+    file_save="/opt/airflow/TRE2026/archives/RR/candidato/jpg"
+)
+
+# ### FIM DA FUNÇÃO / CHAMADAS DAS FUNÇÕES PARA CADA TAREFA ###
+
+    # unzip_local_votacao = unzip_file.override(task_id="unzip_local_votacao_RR")(
+#         zip_path = "/opt/airflow/TRE2026/archives/RR/local_votacao/zip",
+#         file_save="/opt/airflow/TRE2026/archives/RR/local_votacao/csv"
+#     )
+#     local_votacao_RR = local_votacao.override(task_id="etl_local_votacao_RR")(path = '/opt/airflow/TRE2026/archives/RR/local_votacao/csv')
+#     mover_local_votacao_RR = BashOperator(
+#     task_id='mover_local_votacao_RR',
+#     bash_command=f'mv /opt/airflow/TRE2026/archives/RR/local_votacao/zip/*.zip /opt/airflow/TRE2026/archives/RR/local_votacao/processados/'
+#     )
+
+
+# ###-------------------------------------------------------------------------------------------------------------------------------------------------#####
+
+#     unzip_perfil_eleitor = unzip_file.override(task_id="unzip_perfil_eleitor")(
+#     zip_path = "/opt/airflow/TRE2026/archives/RR/perfil_eleitorado_secao/zip",
+#     file_save="/opt/airflow/TRE2026/archives/RR/perfil_eleitorado_secao/csv",
+#     )
+#     perfil_eleitorado_RR = perfil.override(task_id="etl_perfil_eleitorado_RR")(file_csv = '/opt/airflow/TRE2026/archives/RR/perfil_eleitorado_secao/csv')
+#     mover_perfil_eleitorado_RR = BashOperator(
+#     task_id='mover_perfil_eleitorado_RR',
+#     bash_command=f'mv /opt/airflow/TRE2026/archives/RR/perfil_eleitorado_secao/zip/*.zip /opt/airflow/TRE2026/archives/RR/perfil_eleitorado_secao/processados/'
+#     )
+
+# # ###-------------------------------------------------------------------------------------------------------------------------------------------------#####
+#     unzip_resultado_RR = unzip_file.override(task_id="unzip_resultado_RR")(
+#         zip_path = "/opt/airflow/TRE2026/archives/RR/turnos/zip",
+#         file_save="/opt/airflow/TRE2026/archives/RR/turnos/csv",
+#     )
+#     resultado_RR = turnos.override(task_id="etl_resultado_RR")(file_csv ='/opt/airflow/TRE2026/archives/RR/turnos/csv')
+#     mover_resultado_RR = BashOperator(
+#     task_id='mover_resultado_RR',
+#     bash_command=f'mv /opt/airflow/TRE2026/archives/RR/turnos/zip/*.zip /opt/airflow/TRE2026/archives/RR/turnos/processados/'
+    # )
+
+# ###-------------------------------------------------------------------------------------------------------------------------------------------------#####
+
+
+
+
+
+
+#     dbt_cinema = DbtTaskGroup(
+#             group_id="camada_transformacao_dbt",
+#             project_config=ProjectConfig(DBT_PROJECT_PATH),
+#             profile_config=profile_config,
+#             render_config=RenderConfig(
+#                 select=["path:models/cinema"]
+#             ),
+#             operator_args={"install_deps": True},
+#         )
+
+
+    
+    start = EmptyOperator(task_id = 'start')
+    end = EmptyOperator(task_id = 'end')
+
+
+    # start >> unzip_local_votacao >> local_votacao_RR >> mover_local_votacao_RR >> end
+    # start >> unzip_perfil_eleitor >> perfil_eleitorado_RR >> mover_perfil_eleitorado_RR >> end
+    # start >> unzip_resultado_RR >> resultado_RR >> mover_resultado_RR >> end
+    start >> unzip_csv_candidato_RR >> unzip_foto_candidato_RR >> end
+
+eleicao()
